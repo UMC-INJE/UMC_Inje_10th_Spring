4e6e64293912be661ee3f620fbae8cb95558da87
kimjonghn <nick6566@naver.com>
2026-05-04 15:08:42 +0900

    API 명세서를 기반으로 Controller, DTO 제작 응답 통일 객체, 에러 핸들링할 객체를 생성 완료

--- 아공티/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ 아공티/src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,29 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.member.exception.code.MemberSuccessCode;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("v1/users/me")
+    public ApiResponse<MemberResDTO.GetInfo> getinfo(
+            @RequestBody MemberReqDTO.GetInfo dto
+            ){
+        BaseSuccessCode code = MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code, memberService.getInfo(dto));
+    }
 }
--- 아공티/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ 아공티/src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,7 @@
 package com.example.umc10th.domain.member.dto;
 
 public class MemberReqDTO {
+    public record GetInfo(
+            Long id
+    ){}
 }
